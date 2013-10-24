5cc327fd832f281892fe97bc2f5a90bd5cf07e37
Mark Danks <mark.danks@apptivation.co.uk>
2013-10-24 17:25:54 +0100

    Font changes when text setting changes.

--- iOS7 Presentation/Classes/I7PTextSizeViewController.m
+++ iOS7 Presentation/Classes/I7PTextSizeViewController.m
@@ -9,6 +9,13 @@
 #import "I7PTextSizeViewController.h"
 
 @interface I7PTextSizeViewController ()
+@property (strong, nonatomic) IBOutletCollection(UILabel) NSArray *labels;
+@property (weak, nonatomic) IBOutlet UILabel *headline;
+@property (weak, nonatomic) IBOutlet UILabel *subheadline;
+@property (weak, nonatomic) IBOutlet UILabel *body;
+@property (weak, nonatomic) IBOutlet UILabel *caption1;
+@property (weak, nonatomic) IBOutlet UILabel *caption2;
+@property (weak, nonatomic) IBOutlet UILabel *footnote;
 
 @end
 
@@ -27,12 +34,29 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    [[NSNotificationCenter defaultCenter]addObserver:self
+                                            selector:@selector(fontSizeChangedNotification:)
+                                                name:UIContentSizeCategoryDidChangeNotification
+                                              object:nil];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)fontSizeChangedNotification:(NSNotification*)notification{
+    // Annoyingly, font styles in nibs don't refresh automatically
+    self.headline.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+    self.subheadline.font = [UIFont preferredFontForTextStyle:UIFontTextStyleSubheadline];
+    self.body.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    self.caption1.font = [UIFont preferredFontForTextStyle:UIFontTextStyleCaption1];
+    self.caption2.font = [UIFont preferredFontForTextStyle:UIFontTextStyleCaption2];
+    self.footnote.font = [UIFont preferredFontForTextStyle:UIFontTextStyleFootnote];
+    
+    [self.labels makeObjectsPerformSelector:@selector(invalidateIntrinsicContentSize)];
 }
 
 @end
