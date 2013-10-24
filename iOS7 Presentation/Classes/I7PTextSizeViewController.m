3e5d297adc74720ab4f47bf3fd1aab20a734372d
Mark Danks <mark.danks@apptivation.co.uk>
2013-10-24 17:05:08 +0100

    Basic demo of text styles.

---
+++ iOS7 Presentation/Classes/I7PTextSizeViewController.m
@@ -0,0 +1,38 @@
+//
+//  I7PTextSizeViewController.m
+//  iOS7 Presentation
+//
+//  Created by Mark Danks on 24/10/2013.
+//  Copyright (c) 2013 Apptivation. All rights reserved.
+//
+
+#import "I7PTextSizeViewController.h"
+
+@interface I7PTextSizeViewController ()
+
+@end
+
+@implementation I7PTextSizeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
