06e5d7ff1346219bdfcff0cf72b79362fb0a9cb9
Mark Danks <mark.danks@apptivation.co.uk>
2013-10-24 15:23:19 +0100

    Adds a bunch of controls to demo tinting.

--- iOS7 Presentation/Classes/I7PTintingViewController.m
+++ iOS7 Presentation/Classes/I7PTintingViewController.m
@@ -27,6 +27,12 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    // Seed the random number generator (once only)
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        srand48(time(0));
+    });
 }
 
 - (void)didReceiveMemoryWarning
@@ -36,7 +42,14 @@
 }
 
 - (IBAction)tintButtonPressed:(id)sender {
-    
+    self.view.tintColor = [[self class]randomColor];
+}
+
++(UIColor*)randomColor{
+    return [UIColor colorWithRed:drand48()
+                           green:drand48()
+                            blue:drand48()
+                           alpha:1];
 }
 
 @end
